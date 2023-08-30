f20d73ce8978dfad85ca6ba36345b1a77ff2dfce
Anastasia <anastasia.chashch@mail.ru>
2023-08-30 23:45:46 +0500

    done

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+import Collapse from '../components/Collapse/Collapse';
+import CallbackChat from '../components/CallbackChat/CallbackChat';
+import Liker from '../components/Liker/Liker';
+
+const contentEl = document.querySelector('.content');
+const widgetCollapse1 = new Collapse();
+contentEl.prepend(widgetCollapse1.element);
+
+const widgetCollapse2 = new Collapse({ parent: '.content' });
+widgetCollapse2.setText('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae inventore mollitia nesciunt non quos! Accusantium aperiam assumenda eaque impedit, in laborum libero nobis recusandae rem sunt totam, vel, vitae voluptate!');
+
+const callbackChat = new CallbackChat();
+
+const liker = new Liker(contentEl);
